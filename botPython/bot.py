85392db92c82dae8f270a22aafb643391973d1a7
morgannadev <morgcontato@gmail.com>
2024-04-01 10:02:00 -0300

    adicionando projeto com o datapool

--- botPython/bot.py
+++ botPython/bot.py
@@ -10,54 +10,24 @@ def main():
     maestro = BotMaestroSDK.from_sys_args()
     execution = maestro.get_execution()
 
-    # Obtendo o CEP a ser consultado via parâmetro da tarefa
-    cep = execution.parameters.get("cep")
-
     bot = WebBot()
     bot.headless = False
     bot.browser = Browser.FIREFOX
     bot.driver_path = r"resources\geckodriver.exe"
 
     try:
-        maestro.alert(
-            task_id=execution.task_id,
-            title="Iniciando a tarefa",
-            message=f"Iniciando a tarefa de consulta do CEP {cep} nos Correios",
-            alert_type=AlertType.INFO
-        )
+        datapool = maestro.get_datapool("Datapool_Consulta_CEP_Correios")
 
-        bot.browse(LINK_CONSULTA_CEP_CORREIOS)
+        if datapool.is_active():
+            while datapool.has_next():
+                item = datapool.next(task_id=execution.task_id)
+                consulta_cep(item, bot, maestro, execution)
+                item.report_done()
+        else:
+            raise Exception("Datapool não está ativo.")
 
-        campo_endereco = bot.find_element("endereco", By.ID)
-        campo_endereco.send_keys(cep)
-
-        botao_pesquisar = bot.find_element("btn_pesquisar", By.ID)
-        botao_pesquisar.click()
-
-        tabela_enderecos = bot.find_element("resultado-DNEC", By.ID)
-        tabela_enderecos = table_to_dict(tabela_enderecos)[0]
-
-        print(tabela_enderecos)
-
-        bot.wait(100)
-
-        # Registrando log
-        maestro.new_log_entry(
-            activity_label="consulta-cep-correios",
-            values={
-                "cep": cep,
-                "logradouro": tabela_enderecos["logradouronome"],
-                "bairro": tabela_enderecos["bairrodistrito"],
-                "localidade": tabela_enderecos["localidadeuf"]
-            }
-        )      
-
-        # Finalizando tarefa
-        maestro.finish_task(
-            task_id=execution.task_id,
-            status=AutomationTaskFinishStatus.SUCCESS,
-            message="Task finalizada com sucesso!"
-        )
+        status_finalizacao = AutomationTaskFinishStatus.SUCCESS
+        messagem_finalizacao = "Tarefa finalizada com sucesso."
         
     except Exception as erro:
         bot.save_screenshot("erro.png")
@@ -69,16 +39,59 @@ def main():
             screenshot="erro.png"
         )
 
-        # Finalizando tarefa
-        maestro.finish_task(
-            task_id=execution.task_id,
-            status=AutomationTaskFinishStatus.FAILED,
-            message="Task finalizada com erro!"
-        )
+        item.report_error()
+
+        status_finalizacao = AutomationTaskFinishStatus.FAILED
+        messagem_finalizacao = "Tarefa finalizada com erro."
 
     finally:
         bot.stop_browser()
 
+        maestro.finish_task(
+            task_id=execution.task_id,
+            status=status_finalizacao,
+            message=messagem_finalizacao
+        )
+
+def consulta_cep(item: DataPoolEntry, bot: WebBot, maestro: BotMaestroSDK, execution: BotExecution):
+    
+    cep = item["cep"]
+
+    maestro.alert(
+        task_id=execution.task_id,
+        title="Iniciando a tarefa",
+        message=f"Iniciando a tarefa de consulta do CEP {cep} nos Correios",
+        alert_type=AlertType.INFO
+    )
+
+    bot.browse(LINK_CONSULTA_CEP_CORREIOS)
+
+    campo_endereco = bot.find_element("endereco", By.ID)
+    campo_endereco.send_keys(cep)
+
+    botao_pesquisar = bot.find_element("btn_pesquisar", By.ID)
+    botao_pesquisar.click()
+
+    tabela_enderecos = bot.find_element("resultado-DNEC", By.ID)
+    tabela_enderecos = table_to_dict(tabela_enderecos)[0]
+
+    print(tabela_enderecos)
+
+    bot.wait(100)
+
+    # Registrando log
+    maestro.new_log_entry(
+        activity_label="consulta-cep-correios",
+        values={
+            "cep": cep,
+            "logradouro": tabela_enderecos["logradouronome"],
+            "bairro": tabela_enderecos["bairrodistrito"],
+            "localidade": tabela_enderecos["localidadeuf"]
+        }
+    )      
+
+    botao_nova_busca = bot.find_element("btn_nbusca", By.ID)
+    botao_nova_busca.click()
 
 if __name__ == '__main__':
     main()
